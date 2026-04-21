34cd5117080d4aa31d3f4a12ee543ec0fbdca3a4
therealbela <kkristof411@mailbox.unideb.hu>
2026-04-21 12:03:30 +0200

    Delete bead2/tessControlShader.glsl

--- bead2/tessControlShader.glsl
+++
@@ -1,7 +0,0 @@
-#version 430
-layout (vertices = 32) out;
-void main() {
-    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
-    gl_TessLevelOuter[0] = 1.0;
-    gl_TessLevelOuter[1] = 64.0; 
-}
